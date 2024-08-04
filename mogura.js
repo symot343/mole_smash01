7d82bb7cc4f3c09f8cf6d17a037b8eda69aebc23
symot343 <symot343@gmail.com>
2024-08-05 01:42:10 +0900

    Add files via upload

---
+++ mogura.js
@@ -0,0 +1,203 @@
+let can = document.getElementById("can");
+let con = can.getContext("2d");
+
+//画面サイズ
+const SCREEN_W = window.innerWidth;
+const SCREEN_H = window.innerHeight;
+
+//背景黒設定
+can.width = SCREEN_W;
+can.height = SCREEN_H;
+can.style = "background-color:#000000"
+//キャンバスの画質向上
+con.imageSmoothingEnabled = true;
+con.imageSmoothingQuality = 'high';
+
+//フィールドサイズ
+const FIELD_SIZE = 8;
+
+//短い辺の長さ
+let SHORT_SIDE;
+
+//画面のうち短い方の幅
+if (SCREEN_W < SCREEN_H) {
+    SHORT_SIDE = SCREEN_W;
+} else {
+    SHORT_SIDE = SCREEN_H;
+}
+
+//関数の実行頻度(ミリ秒)
+const interval = 100;
+
+//モグラの出現頻度(1秒間に出現するモグラの量)
+let Mole = 8;
+
+//叩いた回数
+let score = 0;
+
+//グリッドの様子
+let grid = new Array(FIELD_SIZE);
+for (let i=0;i<FIELD_SIZE;i++){
+    grid[i] = new Array(FIELD_SIZE).fill(0);
+}
+
+
+//ブロックサイズ，マージンサイズ
+const BLOCK_SIZE = SHORT_SIDE/(FIELD_SIZE+2);
+const MARGIN_SIZE = BLOCK_SIZE;
+
+//フィールドをかく
+function drawField(){
+    con.strokeStyle = "white";
+    con.lineWidth=2;
+    con.strokeRect(MARGIN_SIZE,MARGIN_SIZE,BLOCK_SIZE*FIELD_SIZE,BLOCK_SIZE*FIELD_SIZE);
+    con.lineWidth=3;
+    con.strokeRect(MARGIN_SIZE*0.85, MARGIN_SIZE*0.85, BLOCK_SIZE * (FIELD_SIZE+0.30), BLOCK_SIZE * (FIELD_SIZE+0.30));
+}
+
+
+function initialState(){
+    score = 0;
+    drawField();
+    drawList();
+    drawImages();
+}
+
+//モグラ1の定義
+let mole1 = new Image();
+mole1.src="mole1.png";
+
+//モグラを叩いたときの音
+let smash1 = new Audio("smash.mp3");
+let smash2 = new Audio("smash.mp3");
+let smash3 = new Audio("smash.mp3");
+let smash4 = new Audio("smash.mp3");
+let smash5 = new Audio("smash.mp3");
+
+
+// 画像を描画する関数
+function drawImg(img,x,y) {
+    // 画像がロードされているか確認
+    if (img.complete) {
+        // スムージングを有効化
+        con.imageSmoothingEnabled = true;
+        // 画像をCanvasに描画
+        con.drawImage(img,BLOCK_SIZE*(x+1),BLOCK_SIZE*(y+1), BLOCK_SIZE, BLOCK_SIZE);
+    } else {
+        // 画像がロードされていない場合、ロードイベントで描画を呼び出す
+        mole1.onload = function () {
+            drawImg(mole1,x,y);
+        };
+    }
+}
+
+mole1.onload = function () {
+    drawImg(mole1,1,0);
+};
+
+grid[3][4] = 1;
+grid[4][5] = 1;
+
+//mole1を描くべきリスト
+let mole1ListX = []
+let mole1ListY = []
+
+//mole1を書くべきリストを作る
+function drawList(){
+    mole1ListX = [];
+    mole1ListY = [];
+    for (let i=0;i<FIELD_SIZE;i++){
+        for (let j=0;j<FIELD_SIZE;j++){
+            if ((grid[i][j] >= 1) && (grid[i][j] <= 10)) {
+                mole1ListX.push(i);
+                mole1ListY.push(j);
+            }
+        }
+    }
+}
+
+function drawImages(){
+    con.clearRect(MARGIN_SIZE, MARGIN_SIZE, BLOCK_SIZE * FIELD_SIZE, BLOCK_SIZE * FIELD_SIZE)
+    for (let i = 0; i < mole1ListX.length; i++) {
+        drawImg(mole1, mole1ListX[i], mole1ListY[i]);
+    }
+}
+
+function drawStatus(){
+    con.clearRect(0,0,can.width,MARGIN_SIZE*0.80);
+    con.font = "20px Arial";
+    con.fillStyle = "white";
+    con.textAlign = "center";
+    con.textBaseline = "middle";
+    con.fillText(`Score:${score}`,can.width/2,MARGIN_SIZE/2);
+}
+
+document.getElementById("can").onclick = function(e){
+    let clickX = e.clientX;
+    let clickY = e.clientY;
+    let x = Math.floor(clickX/BLOCK_SIZE) - 1;
+    let y = Math.floor(clickY/BLOCK_SIZE) - 1;
+    if ((grid[x][y]>=1)&&(grid[x][y]<=10)){
+        grid[x][y]==0;
+        score++;
+        drawList();
+        drawImages();
+        drawStatus();
+        if (smash1.paused){
+            smash1.play();
+        }
+        else if (smash2.paused) {
+            smash2.play();
+        }
+        else if (smash3.paused) {
+            smash3.play();
+        }
+        else if (smash4.paused) {
+            smash4.play();
+        }
+        else if (smash5.paused) {
+            smash5.play();
+        }
+    }
+}
+
+//グリッドのモグラの状態を変化させる
+function nextGrid(){
+    for (let i=0;i<FIELD_SIZE;i++){
+        for (let j=0;j<FIELD_SIZE;j++){
+            if ((grid[i][j] >= 1) && (grid[i][j] < 10)) {
+                grid[i][j]++;
+            }
+            else if (grid[i][j]==10){
+                grid[i][j]=0;
+            }
+        }
+    }
+}
+
+//ランダムにモグラを出現させる
+function emergeMole(){
+    for (let i = 0; i < FIELD_SIZE; i++) {
+        for (let j = 0; j < FIELD_SIZE; j++) {
+            if (grid[i][j]==0){
+                let num = Math.random();
+                if (num<=((Mole*(interval/1000))/(FIELD_SIZE*FIELD_SIZE))){
+                    grid[i][j]=1;
+                }
+            }
+        }
+    }
+}
+
+function game(){
+    nextGrid();
+    emergeMole();
+    drawList();
+    drawImages();
+    drawStatus();
+}
+
+initialState();
+setInterval(game,interval);
+
+
